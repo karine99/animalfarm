45344346b8cc59de57f19e093cd221d797201003
Karine Van Mellaert <BE020636@barry-callebaut.com>
2015-10-27 10:33:32 +0100

    oefening 1:add gans

---
+++ src/com/company/Gans.java
@@ -0,0 +1,16 @@
+package com.company;
+
+/**
+ * Created by BE020636 on 27/10/2015.
+ */
+public class Gans implements Farmable {
+    @Override
+    public String makeNoise() {
+        return ("snater snater, kwetter kwetter");
+    }
+
+    @Override
+    public void move() {
+// waggel waggel
+    }
+}
